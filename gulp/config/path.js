991a948c0739c86b1d20a109e5d4a483f1a8a850
Mikhail Ivanov <ivamish051@gmail.comm>
2022-03-12 16:33:48 +0800

    init project

---
+++ gulp/config/path.js
@@ -0,0 +1,26 @@
+import * as pathNode from 'path';
+
+const _SRC_ = './src',
+      _DIST_ = './dist';
+
+export const path = {
+    src: {
+        files: [`${_SRC_}/img/**/*.*`,
+                `${_SRC_}/icons/**/*.*`,],
+        html: `${_SRC_}/*.html`,
+        css: `${_SRC_}/scss/**/*.scss`,
+        js: `${_SRC_}/js/main.js`
+    },
+    dist: {
+        files: `${_DIST_}`,
+        html: `${_DIST_}`,
+        css: `${_DIST_}/css`,
+        js: `${_DIST_}/js`
+    },
+    watch: {
+        files: `${_SRC_}/assets/`,
+        html: `${_SRC_}/**/*.html`,
+        css: `${_SRC_}/scss/**/*.scss`,
+        js: `${_SRC_}/js/**/*.js`
+    },
+};

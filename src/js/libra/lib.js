14d12e6e63eac59c39ec685e749d1e4185bdb9b9
Mikhail Ivanov <ivamish051@gmail.comm>
2022-03-14 21:07:27 +0800

    added actions methods

--- src/js/libra/lib.js
+++ src/js/libra/lib.js
@@ -2,5 +2,7 @@ import { $ } from "./core.js";
 import './modules/display.js';
 import './modules/classes.js';
 import './modules/listeners.js';
+import './modules/actions.js';
+import './modules/effects.js';
 
 export default $;
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 import $ from './libra/lib.js';
 
-$('div').click((e) => {
-    $(e.target).hide();
-    // console.log($(e.target));
-});
+// $('div').eq(0).find('.some').hide();
+// console.log($('.some'));
+console.log($('.some').eq(3).siblings());
